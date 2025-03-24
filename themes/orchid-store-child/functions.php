e2cde0552f2500317ac40503182f512f612296bb
danh126 <=>
2025-03-24 18:14:05 +0700

    Updated custom Orchid Store Child theme

--- themes/orchid-store-child/functions.php
+++ themes/orchid-store-child/functions.php
@@ -1,5 +1,8 @@
 <?php
-//================ Thiết lập ban đầu ====================
+
+/**
+ * Thiết lập ban đầu
+ */
 
 // Kế thừa CSS từ theme gốc và thêm assets từ child theme
 function orchid_store_child_enqueue_assets()
@@ -21,7 +24,9 @@ function orchid_store_child_enqueue_assets()
 }
 add_action('wp_enqueue_scripts', 'orchid_store_child_enqueue_assets');
 
-//================ Demo test ====================
+/**
+ * Demo test
+ */
 
 // Thêm thẻ meta vào head
 function custom_add_meta_tag()
@@ -50,7 +55,9 @@ function add_login_logout_link($items, $args)
 }
 add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2); // 10 -> mức độ ưu tiên, 2 -> tham số nhận vào function
 
-//================ Điều hướng đăng nhập & đăng xuất ====================
+/**
+ * Điều hướng đăng nhập & đăng xuất
+ */
 
 // Khi đăng nhập điều hướng theo vai trò
 function custom_login_redirect($redirect_to, $request, $user)
@@ -82,7 +89,9 @@ function custom_logout_redirect($redirect_to, $requested_redirect_to, $user)
 add_filter('logout_redirect', 'custom_logout_redirect', 10, 3); // 10 -> mức độ ưu tiên, 3 -> tham số nhận vào function
 
 
-//================ Xử lý hiển thị thông báo đăng nhập ====================
+/**
+ * Xử lý hiển thị thông báo đăng nhập
+ */
 
 // Lưu thông báo sau khi đăng nhập
 function welcome_user_login($user_login, $user)
@@ -104,3 +113,37 @@ function show_welcome_message()
     get_template_part('template-parts/notifications/welcome-message');
 }
 add_action('wp_footer', 'show_welcome_message');
+
+/**
+ * Thao tác với API
+ */
+
+// Add custom field
+function add_custom_field_to_rest_api()
+{
+    // Thêm trường views_count vào API của bài viết
+    register_rest_field('post', 'views_count', array(
+        // Trả về giá trị views_count của bài viết
+        'get_callback' => function ($post) {
+            return get_post_meta($post['id'], 'views_count', true);
+        },
+
+        // Cho phép cập nhật giá trị views_count thông qua REST API
+        'update_callback' => function ($value, $post) {
+            return update_post_meta($post->ID, 'views_count', sanitize_text_field($value));
+        },
+        'schema' => null
+    ));
+}
+add_action('rest_api_init', 'add_custom_field_to_rest_api');
+
+// Block api users
+function disable_rest_users_endpoint($endpoints)
+{
+    if (isset($endpoints['/wp/v2/users'])) {
+        unset($endpoints['/wp/v2/users']);
+    }
+
+    return $endpoints;
+}
+add_filter('rest_endpoints', 'disable_rest_users_endpoint');
